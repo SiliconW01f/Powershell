4a436e5b70e83af901ef15d198cac00a954da018
SiliconW01f <31215734+SiliconW01f@users.noreply.github.com>
2018-06-21 22:53:24 -0400

    Add files via upload

---
+++ RecursiveHash.ps1
@@ -0,0 +1,32 @@
+Clear-Host
+
+$hash=Read-Host "Please enter an MD5 hash value to search"
+$outputfolder="C:\test"
+$outputfile="hashes.txt"
+
+function HashTheFiles {
+
+Get-ChildItem / -Recurse -ErrorAction SilentlyContinue | Get-FileHash -Algorithm MD5 -ErrorAction SilentlyContinue | Tee $outputfolder\$outputfile | Select-String "$hash"
+
+}
+
+function SearchForHash {
+Write-Host "`n"
+Write-Host "Scanning existing hashes - to rehash files, delete $outputfolder\$outputfile"
+Write-Host "`n"
+Get-Content $outputfolder\$outputfile | Select-String "$hash"
+
+}
+
+New-Item -ItemType Directory -Force -Path $outputfolder > $null
+
+if (Test-Path $outputfolder\$outputfile)
+{
+
+    SearchForHash    
+} else {
+    HashTheFiles	
+
+}
+
+Pause
